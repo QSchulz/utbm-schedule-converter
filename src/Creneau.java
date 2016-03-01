d70bcb1f7d61ef75768859bce36c13178b4e121f
Quentin Schulz <q.schulz@gmail.com>
2016-03-01 20:02:27 +0100

    Version 0.1

---
+++ src/Creneau.java
@@ -0,0 +1,93 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Creneau {
+
+	private String site = null;
+	private String codeUV = null;
+	private String type = null;
+	private int groupe = 1;
+	private String jour = null;
+	private String heureDebut = null;
+	private String heureFin = null;
+	private int frequence = 1;
+	private String salle = null;
+
+	public Creneau(String line) {
+		int i = 1;
+		String[] infos = line.split(" ");
+		this.site = infos[0];
+		while(infos[i].isEmpty()) {
+			i++;
+		}
+		this.codeUV = infos[i++];
+		while(infos[i].isEmpty()) {
+			i++;
+		}
+		this.type = infos[i++];
+		while(infos[i].isEmpty()) {
+			i++;
+		}
+		this.groupe = Integer.parseInt(infos[i++]);
+		while(infos[i].isEmpty()) {
+			i++;
+		}
+		this.jour = infos[i++];
+		while(infos[i].isEmpty()) {
+			i++;
+		}
+		this.heureDebut = infos[i++];
+		while(infos[i].isEmpty()) {
+			i++;
+		}
+		this.heureFin = infos[i++];
+		while(infos[i].isEmpty()) {
+			i++;
+		}
+		this.frequence = Integer.parseInt(infos[i++]);
+		while(infos[i].isEmpty()) {
+			i++;
+		}
+		this.salle = infos[i];
+	}
+
+	private String getOldDay() {
+		if (this.jour.startsWith("m")) {
+			return this.jour.substring(0,2).toUpperCase();
+		}
+		return this.jour.substring(0,1).toUpperCase();
+	}
+
+	private String getOldTime(String time) {
+		return time.replace(":", "H");
+	}
+
+	public String printOld() {
+		return this.codeUV
+				+ " "
+				+ (this.type.equals("CM") ? "C " : this.type)
+				+ this.groupe
+				+ "  : "
+				+ this.getOldDay()
+				+ " "
+				+ this.getOldTime(this.heureDebut)
+				+ "-"
+				+ this.getOldTime(this.heureFin)
+				+ (this.frequence > 1 ? " (1 SEMAINE / " + this.frequence + ")"
+						: "") + " en " + this.salle;
+	}
+
+	public String printNew() {
+		List<String> strings = new ArrayList<String>();
+		strings.add(this.site);
+		strings.add(this.codeUV);
+		strings.add(this.type + " " + this.groupe);
+		strings.add(this.jour);
+		strings.add(this.heureDebut);
+		strings.add(this.heureFin);
+		strings.add("" + this.frequence);
+		strings.add(this.salle);
+		return String.join("\t", strings);
+	}
+
+}
---
+++ src/Main.java
@@ -0,0 +1,51 @@
+import java.awt.Menu;
+import java.awt.MenuBar;
+import java.awt.MenuItem;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
+public class Main extends JFrame implements ActionListener {
+
+	private JTextArea textArea = new JTextArea(
+			"Colle ton emploi du temps ici petit(e) UTBohéMien(ne), puis clique sur Menu->Convertir.");
+	private MenuBar mbar = new MenuBar();
+	private Menu menu = new Menu("Menu");
+	private MenuItem convert = new MenuItem("Convertir");
+	
+	public Main() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.add(textArea);
+		mbar.add(menu);
+		menu.add(convert);
+		menu.addActionListener(this);
+		this.setMenuBar(mbar);
+		this.setTitle("UTBM Schedule Converter");
+		this.setSize(600,400);
+		this.setVisible(true);
+	}
+
+	private void translate() {
+		String oldSchedule = "";
+		String[] lines = this.textArea.getText().split("\n");
+		for (String line : lines) {
+			if (line.startsWith("Sevenans") || line.startsWith("Belfort")
+					|| line.startsWith("Montb")) {
+				oldSchedule += new Creneau(line).printOld() + "\n";
+			}
+		}
+		
+		this.textArea.setText(oldSchedule);
+		
+	}
+
+	public static void main(String[] args) {
+		new Main();
+	}
+
+	public void actionPerformed(ActionEvent arg0) {
+		this.translate();		
+	}
+}
